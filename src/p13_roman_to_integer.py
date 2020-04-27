03a4a032ae7d07ee53608cffb61027ab46098c53
jcglqmoyx <jcglqmoyx@gmail.com>
2020-04-27 23:00:01 +0800

    add problems

---
+++ src/p13_roman_to_integer.py
@@ -0,0 +1,17 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        values = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+        sum = 0
+        for i in range(len(s)):
+            if i < len(s) - 1 and values[s[i]] < values[s[i + 1]]:
+                sum -= values[s[i]]
+            else:
+                sum += values[s[i]]
+        return sum
+
+
+print(Solution().romanToInt('III'))
+print(Solution().romanToInt('IV'))
+print(Solution().romanToInt('IX'))
+print(Solution().romanToInt('LVIII'))
+print(Solution().romanToInt('MCMXCIV'))
---
+++ src/p20_valid_parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        map = {')': '(', ']': '[', '}': '{'}
+        stack = []
+        for i in range(len(s)):
+            ch = s[i]
+            if map.__contains__(ch):
+                if not stack or stack[-1] != map[ch]:
+                    return False
+                else:
+                    stack.pop()
+            else:
+                stack.append(ch)
+        return not stack
+
+
+print(Solution().isValid('()'))
+print(Solution().isValid('()[]{}'))
+print(Solution().isValid('(]'))
+print(Solution().isValid('([)]'))
+print(Solution().isValid('(){[]}'))
