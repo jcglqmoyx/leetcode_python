e8d76e2e47d164c58a36f336aa192deff1b5f957
jcglqmoyx <jcglqmoyx@gmail.com>
2020-04-28 20:35:43 +0800

    add problems

---
+++ src/p771_jewels_and_stones.py
@@ -0,0 +1,22 @@
+# class Solution:
+#     def numJewelsInStones(self, J: str, S: str) -> int:
+#         map = {}
+#         for ch in J:
+#             map[ch] = 1
+#         count = 0
+#         for ch in S:
+#             if map.__contains__(ch):
+#                 count += 1
+#         return count
+
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        count = 0
+        for ch in S:
+            if ch in J:
+                count += 1
+        return count
+
+
+print(Solution().numJewelsInStones('aA', 'aAAbbbb'))
+print(Solution().numJewelsInStones('z', 'ZZ'))
