b9ac145964a51e341c2bbb570379fd0b5fedbe5d
jcglqmoyx <jcglqmoyx@gmail.com>
2020-05-01 08:21:31 +0800

    remove test cases

--- src/p1160_find_words_that_can_be_formed_by_characters.py
+++ src/p1160_find_words_that_can_be_formed_by_characters.py
@@ -1,6 +1,13 @@
 from typing import List, Dict
 
 
+def form_string(word: str, map: Dict[str, int]) -> bool:
+    for ch in word:
+        if not map.__contains__(ch) or word.count(ch) > map[ch]:
+            return False
+    return True
+
+
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
         length = 0
@@ -8,13 +15,6 @@ class Solution:
         for c in chars:
             map[c] = chars.count(c)
         for word in words:
-            if self.form_string(word, map):
+            if form_string(word, map):
                 length += len(word)
         return length
-
-    def form_string(self, word: str, map: Dict[str, int]) -> bool:
-        count = {}
-        for ch in word:
-            if word.count(ch) > map[ch]:
-                return False
-        return True
